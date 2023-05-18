8d3945907994c661367f7672c8b62ea0fdffaf97
YIHAN <105974692+neil0x46dc@users.noreply.github.com>
2023-05-18 15:37:15 +0800

    Initial Commit

---
+++ Reddio-SDK-Example/Reddio_SDK_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reddio_SDK_ExampleApp.swift
+//  Reddio-SDK-Example
+//
+//  Created by Yihan Li on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Reddio_SDK_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
