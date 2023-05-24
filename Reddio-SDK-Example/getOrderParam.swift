e1e358b87086d9273ae73480f0211c2bb71d3905
YIHAN <105974692+neil0x46dc@users.noreply.github.com>
2023-05-24 08:49:19 +0800

    Create getOrderParam.swift

---
+++ Reddio-SDK-Example/getOrderParam.swift
@@ -0,0 +1,79 @@
+//
+//  getOrderParam.swift
+//  Reddio-SDK-Example
+//
+//  Created by Yihan Li on 5/24/23.
+//
+
+import Foundation
+import Alamofire // for network requests
+import BigInt // for big integer manipulation
+
+struct OrderParams {
+    // Define the structure according to JavaScript's OrderParams structure.
+    // For example:
+    var orderType: String
+    var price: String
+    var amount: String
+    var keypair: Keypair
+    // Add other properties...
+}
+
+struct OrderRequestParams {
+    // Define the structure according to JavaScript's OrderRequestParams structure.
+    // For example:
+    var quoteToken: String
+    var amount: String
+    // Add other properties...
+}
+
+struct SignOrderParams {
+    // Define the structure according to JavaScript's SignOrderParams structure.
+    // Add necessary properties...
+}
+
+struct Keypair {
+    var publicKey: String
+    var privateKey: String
+}
+
+func getOrderParams(request: Alamofire.Session, params: OrderParams, completion: @escaping (Result<OrderRequestParams, Error>) -> Void) {
+    // Gather parameters
+    let orderType = params.orderType
+    let price = params.price
+    let amount = params.amount
+    let keypair = params.keypair
+
+    let starkKey = keypair.publicKey
+
+    // Build the info request
+    let infoRequest = ""// construct your info request as needed
+
+    request.request(infoRequest).validate().responseJSON { response in
+        // Parse the response
+        switch response.result {
+        case .success(let value):
+            guard let result = value as? [String: Any],
+                  let data = result["data"] as? [String: Any],
+                  let vault_ids = data["vault_ids"] as? [String],
+                  let asset_ids = data["asset_ids"] as? [String],
+                  let quoteToken = asset_ids[1] as? String else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to parse response"])))
+                return
+            }
+
+            let direction = orderType == "buy" ? 1 : 0
+            let amountBuy = BigUInt(price)! * BigUInt(amount)!
+            let formatPrice = BigUInt(price)!
+
+            // Define other variables and build `partParams` dictionary...
+
+            // Define signOrderParams and call signOrder function...
+
+            // After all calculations and operations, call the completion handler
+            completion(.success(OrderRequestParams(/* initialize with needed data */)))
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
+}
